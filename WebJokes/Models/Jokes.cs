b7c5a1b95ca409b4391febee402750ada913cba5
52696 <52696@aebatalha.ccems.pt>
2023-11-03 14:12:26 +0000

    Adicionar arquivos de projeto.

---
+++ WebJokes/Models/Jokes.cs
@@ -0,0 +1,17 @@
+﻿namespace WebJokes.Models
+{
+    public class Jokes
+    {
+        public int ID { get; set; }
+        public string JokePergunta { get; set; }
+        public string JokeResposta { get; set; }
+
+        public Jokes()
+        {
+            ID = 0;
+            JokePergunta = string.Empty;
+            JokeResposta = string.Empty;
+
+        }
+    }
+}
